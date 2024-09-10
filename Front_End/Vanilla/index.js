9e6b0204516b3a491cf14d9d37ff7f9b0f362716
Yashvardhan Ashok <yashvardhan577@gmail.com>
2024-09-10 23:37:17 +0530

    doc

---
+++ Front_End/Vanilla/index.js
@@ -0,0 +1,8 @@
+// menu
+document.getElementById('menu').addEventListener('click', () => {
+  document.querySelector('.menu_cu').classList.toggle('active');
+  document.querySelector('.nav_box_on_click').classList.toggle('active');
+
+});
+
+
